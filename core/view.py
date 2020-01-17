185fd4e6a2ea1ba1dbcc45fe90c703bb60176893
Felix Schulthess <fsch@truthbox.ch>
2020-01-17 14:11:50 +0100

    Remove dead code

--- core/view.py
+++ core/view.py
@@ -2,17 +2,20 @@
 
 import itertools as it
 from core.game import SquareGame
-from core.board import SquareBoard
 
 class View():
+
+    FIELD = ["┼────┼",
+             "│    │",
+             "┼────┼"]
 
     def __init__(self, game):
         self._game = game
 
     def draw(self, stdscr, atoms_visible=False):
         # Offsets for drawing tiled fields
-        fsy, fsx = (len(SquareBoard.FIELD)-1,
-                len(SquareBoard.FIELD[0])-1)
+        fsy, fsx = (len(self.FIELD)-1,
+                len(self.FIELD[0])-1)
         # Draw fields one by one
         for (y, x) in it.product(range(1,9), repeat=2):
             self.draw_field(stdscr, fsy*y, fsx*x)
@@ -28,8 +31,8 @@ class View():
         """
         labels = map(chr, it.cycle(range(65,91)))
         # Offsets for drawing tiled fields
-        fsy, fsx = (len(SquareBoard.FIELD)-1,
-                len(SquareBoard.FIELD[0])-1)
+        fsy, fsx = (len(self.FIELD)-1,
+                len(self.FIELD[0])-1)
         for start, end in self._game.probes:
             label = labels.__next__()
             y, x = start
@@ -42,7 +45,7 @@ class View():
 
     def draw_field(self, stdscr, y, x, label=None, background=True):
         if(background):
-            for y_offset, row in enumerate(SquareBoard.FIELD):
+            for y_offset, row in enumerate(self.FIELD):
                 stdscr.addstr(y+y_offset, x, row)
         if label:
             stdscr.addnstr(y+1, x+1, label, 4)
